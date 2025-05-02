981adda84c68ac20d12bbd6ef4e3c08c2e1eaac3
Ivan <karaberov.ivan@yandex.ru>
2025-05-02 14:34:47 +0000

    refactor: getIP moved to utils

--- middleware/rate_limiter.go
+++ middleware/rate_limiter.go
@@ -3,16 +3,15 @@ package middleware
 import (
 	"gBalancer/errors"
 	"gBalancer/ratelimiter"
-	"net"
+	"gBalancer/utils"
 	"net/http"
-	"strings"
 )
 
 // Creates middleware for token management.
 func RateLimitMiddleware(manager *ratelimiter.TokenBucketManager) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			clientIP := getIP(r)
+			clientIP := utils.GetIP(r)
 
 			bucket, err := manager.GetBucket(clientIP, true)
 			if err != nil {
@@ -28,20 +27,4 @@ func RateLimitMiddleware(manager *ratelimiter.TokenBucketManager) func(http.Hand
 			next.ServeHTTP(w, r)
 		})
 	}
-}
-
-// Retrieves the client's IP address from the HTTP request.
-func getIP(r *http.Request) string {
-	ip := r.Header.Get("X-Forwarded-For")
-	if ip != "" {
-		ips := strings.Split(ip, ",")
-		return strings.TrimSpace(ips[0])
-	}
-
-	ip = r.RemoteAddr
-	host, _, err := net.SplitHostPort(ip)
-	if err != nil {
-		return ip
-	}
-	return host
 }
