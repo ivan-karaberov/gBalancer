bedc16a00cb2a9be2654cf65d1a90bb31b4c965d
Ivan <karaberov.ivan@yandex.ru>
2025-05-02 13:14:56 +0000

    feat: add /endpoint handlers

--- main.go
+++ main.go
@@ -2,33 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 
 	"gBalancer/balancer"
 	"gBalancer/config"
+	"gBalancer/controllers"
+	"gBalancer/logger"
+	"gBalancer/middleware"
+	"gBalancer/models"
+	"gBalancer/ratelimiter"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
+	logrus.SetFormatter(&logger.CustomFormatter{})
+
 	config, err := config.ReadConfig("config.json")
 	if err != nil {
-		log.Fatalf("failed read balancer config > %s", err)
+		logrus.Fatalf("failed read balancer config > %s", err)
 	}
 
 	backendPool, err := balancer.NewBackendPool(config.Backends, &balancer.RoundRobin{})
 	if err != nil {
-		log.Fatalf("failed create balancer > %s", err)
+		logrus.Fatalf("failed create balancer > %s", err)
 	}
+
+	db := models.NewDBConnection()
+
+	models.UpdateAllClient(db, config.RatePerSec, config.Capacity)
+
+	bucketManager := ratelimiter.NewTokenBucketManager(db, config)
+
+	mux := http.NewServeMux()
+	mux.Handle("/clients/", http.HandlerFunc(controllers.ClientHandler(db)))
+	mux.Handle("/", middleware.RateLimitMiddleware(bucketManager)(http.HandlerFunc(backendPool.HandleRequest)))
 
 	server := http.Server{
-		Addr:    fmt.Sprintf("%s:%d", "localhost", config.Port),
-		Handler: http.HandlerFunc(backendPool.HandleRequest),
+		Addr:    fmt.Sprintf(":%d", config.Port),
+		Handler: mux,
 	}
 
-	go backendPool.HealthCheck()
+	stopChan := make(chan struct{})
+	go backendPool.HealthCheck(time.Minute*2, stopChan)
+	defer close(stopChan)
 
-	log.Printf("Load Balancer started at :%d\n", config.Port)
+	logrus.Infof("Load Balancer started at :%d", config.Port)
 	if err := server.ListenAndServe(); err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 }
